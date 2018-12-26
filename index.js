df5ad0b9ac0bc1d5a392e7c36afda618966f8db0
Shreekar Hegde <shreekarhegde9@gmail.com>
2018-12-26 16:15:37 +0530

    Removed extra config from index

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(cors());
 
 app.use('/', routes);
 
-
 server = app.listen(port2, function(){
     console.log('server is running on port 8080')
 });
@@ -25,18 +24,6 @@ io.on('connection', (socket) => {
     socket.on('SEND_MESSAGE', function(data){
         io.emit('RECEIVE_MESSAGE', data);
     })
-});
-
-// ... other imports 
-const path = require("path")
-
-// ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
-
-// ...
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(port, function(){
