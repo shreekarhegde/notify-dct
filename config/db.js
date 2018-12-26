9a1285c388bac2f25d945ec2b4dc45fd005ce0c9
Shreekar Hegde <shreekarhegde9@gmail.com>
2018-12-26 16:26:01 +0530

    template string in db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise;
 
 const CONNECTION_URI = process.env.MONGOLAB_URI;
 
-mongoose.connect(CONNECTION_URI, {
+mongoose.connect(`${CONNECTION_URI}`, {
     useNewUrlParser: true}).then(() => {
         console.log('connected to db');
     }).catch((err) => {
